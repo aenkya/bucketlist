a9e28de26729596be706481a6e8041aa036640fd
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2017-10-09 11:21:41 +0300

    Add link to all results

--- src/app/shared/core/nobuckets.component/nobuckets.component.ts
+++ src/app/shared/core/nobuckets.component/nobuckets.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { User } from '../../../shared/models/user';
+import { BucketlistService } from '../../../shared/bucketlist.service';
 
 @Component({
   templateUrl: 'nobuckets.component.html',
@@ -15,11 +16,15 @@ export class NoBucketsComponent implements OnInit {
   @Input() isItem: boolean;
   @Input() isSearchData: boolean;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private bucketlistService: BucketlistService) {
     this.nobucketlists = true;
   }
 
   ngOnInit(): void {
 
+  }
+
+  push() {
+    this.bucketlistService.primaryStream.next(true);
   }
 }
